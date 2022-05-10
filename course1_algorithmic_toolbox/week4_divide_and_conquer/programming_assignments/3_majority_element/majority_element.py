c96649850b2bdb874af2001d9b944200d11f8289
Vaibhav Vikas <vbhvvikas@gmail.com>
2022-05-10 23:08:16 +0530

    Added majority_element using divide and conquer

--- course1_algorithmic_toolbox/week4_divide_and_conquer/programming_assignments/3_majority_element/majority_element.py
+++ course1_algorithmic_toolbox/week4_divide_and_conquer/programming_assignments/3_majority_element/majority_element.py
@@ -3,16 +3,32 @@ import sys
 
 def get_majority_element(a, left, right):
     if left == right:
-        return -1
-    if left + 1 == right:
         return a[left]
-    #write your code here
+
+    mid = left + (right - left)//2
+    left_maj = get_majority_element(a, left, mid)
+    right_maj = get_majority_element(a, mid + 1, right)
+
+    c1, c2 = 0, 0
+
+    for i in a[left:right+1]:
+        if i == left_maj:
+            c1 += 1
+        elif i == right_maj:
+            c2 += 1
+
+    if c1 > (right - left + 1)//2 and left_maj != -1:
+        return left_maj
+    elif c2 > (right - left + 1)//2 and right_maj != -1:
+        return right_maj
+
     return -1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, 0, n-1) != -1:
         print(1)
     else:
         print(0)
