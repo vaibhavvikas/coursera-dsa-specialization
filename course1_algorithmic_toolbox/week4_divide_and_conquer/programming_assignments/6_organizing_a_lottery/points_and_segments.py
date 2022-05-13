88c5bfa448c1f595357d2f43eb05eb29b8f64221
Vaibhav Vikas <vbhvvikas@gmail.com>
2022-05-13 23:54:59 +0530

    points_and_segments.py

--- course1_algorithmic_toolbox/week4_divide_and_conquer/programming_assignments/6_organizing_a_lottery/points_and_segments.py
+++ course1_algorithmic_toolbox/week4_divide_and_conquer/programming_assignments/6_organizing_a_lottery/points_and_segments.py
@@ -1,18 +1,24 @@
 # Uses python3
 import sys
 
+
 def fast_count_segments(starts, ends, points):
-    cnt = [0] * len(points)
-    #write your code here
+    cnt = {}
+    points_list = [[each, "l"] for each in starts]\
+        + [[each, "r"] for each in ends]\
+        + [[each, "p"] for each in points]
+
+    points_list.sort()
+    no_of_segments = 0
+    for each in points_list:
+        if each[1] == "l":
+            no_of_segments += 1
+        elif each[1] == "r":
+            no_of_segments -= 1
+        else:
+            cnt[each[0]] = no_of_segments
     return cnt
 
-def naive_count_segments(starts, ends, points):
-    cnt = [0] * len(points)
-    for i in range(len(points)):
-        for j in range(len(starts)):
-            if starts[j] <= points[i] <= ends[j]:
-                cnt[i] += 1
-    return cnt
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -20,9 +26,8 @@ if __name__ == '__main__':
     n = data[0]
     m = data[1]
     starts = data[2:2 * n + 2:2]
-    ends   = data[3:2 * n + 2:2]
+    ends = data[3:2 * n + 2:2]
     points = data[2 * n + 2:]
-    #use fast_count_segments
-    cnt = naive_count_segments(starts, ends, points)
-    for x in cnt:
-        print(x, end=' ')
+    cnt = fast_count_segments(starts, ends, points)
+    for x in points:
+        print(cnt[x], end=' ')
