480d219c7f56f8948b893c200d7715f90dace61b
Vaibhav Vikas <vbhvvikas@gmail.com>
2022-05-15 16:03:15 +0530

    Added lcs2.py and lcs3.py

--- course1_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ course1_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -1,23 +1,35 @@
-#Uses python3
+# python3
+import numpy
 
-import sys
+def lcs3(first_sequence, second_sequence, third_sequence):
+    assert len(first_sequence) <= 100
+    assert len(second_sequence) <= 100
+    assert len(third_sequence) <= 100
 
-def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    dp_list = numpy.zeros((len(first_sequence) + 1, len(second_sequence) + 1, len(third_sequence) + 1))
+
+    for i in range(1, len(first_sequence) + 1):
+        for j in range(1, len(second_sequence) + 1):
+            for k in range(1, len(third_sequence) + 1):
+                if first_sequence[i - 1] == second_sequence[j - 1] == third_sequence[k - 1]:
+                    dp_list[i][j][k] = dp_list[i - 1][j - 1][k - 1] + 1
+                else:
+                    dp_list[i][j][k] = max(dp_list[i - 1][j][k], dp_list[i][j - 1][k], dp_list[i][j][k-1])
+
+    return int(dp_list[-1][-1][-1])
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    an = data[0]
-    data = data[1:]
-    a = data[:an]
-    data = data[an:]
-    bn = data[0]
-    data = data[1:]
-    b = data[:bn]
-    data = data[bn:]
-    cn = data[0]
-    data = data[1:]
-    c = data[:cn]
+    n = int(input())
+    a = list(map(int, input().split()))
+    assert len(a) == n
+
+    m = int(input())
+    b = list(map(int, input().split()))
+    assert len(b) == m
+
+    q = int(input())
+    c = list(map(int, input().split()))
+    assert len(c) == q
+
     print(lcs3(a, b, c))
